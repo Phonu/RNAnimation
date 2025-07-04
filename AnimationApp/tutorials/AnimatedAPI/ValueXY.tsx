f19fc6f60f58038ee2ce23cb2dcbb9af048d5a98
Phonu <50923254+Phonu@users.noreply.github.com>
2025-07-04 11:38:29 +0530

    oscillation animation

--- AnimationApp/tutorials/AnimatedAPI/ValueXY.tsx
+++ AnimationApp/tutorials/AnimatedAPI/ValueXY.tsx
@@ -10,11 +10,13 @@ import React, { useEffect, useRef } from "react";
 const ValueXY = () => {
   // V C F
 
+  //   const position1 = useAnimatedValue(0);
+  //   const position2 = useAnimatedValue(15);
+  //   const combinedPosition = Animated.add(position1, position2);
+
   //   const position = useRef(new Animated.Value(0)).current;
   const position = useAnimatedValue(0);
-
   const xyValue = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
-
   const startAnimationXy = () => {
     Animated.timing(xyValue, {
       toValue: { x: 50, y: -150 },
@@ -23,9 +25,29 @@ const ValueXY = () => {
     }).start();
   };
 
+  const oscillation = useAnimatedValue(0);
+  const startOscillation = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(oscillation, {
+          toValue: 100,
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(oscillation, {
+          toValue: 0,
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+      ]),
+      { iterations: 5 }
+    ).start();
+  };
+
   useEffect(() => {
     // startAnimation();
-    startAnimationXy();
+    // startAnimationXy();
+    startOscillation();
   }, []);
 
   return (
@@ -35,6 +57,10 @@ const ValueXY = () => {
       {/* <Animated.View style={[styles.box2, xyValue.getLayout()]} /> */}
       <Animated.View
         style={[styles.box2, { transform: xyValue.getTranslateTransform() }]}
+      />
+      {/*  oscillation animation using loop and sequence */}
+      <Animated.View
+        style={[styles.circle, { transform: [{ translateX: oscillation }] }]}
       />
     </View>
   );
@@ -51,6 +77,13 @@ const styles = StyleSheet.create({
     height: 100,
     width: 100,
     backgroundColor: "orange",
+    marginBottom: 8,
+  },
+  circle: {
+    height: 100,
+    width: 100,
+    borderRadius: 100,
+    backgroundColor: "green",
     marginBottom: 8,
   },
 });
